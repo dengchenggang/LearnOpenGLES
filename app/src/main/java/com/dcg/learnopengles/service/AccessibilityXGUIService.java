eed49104a4fde9def74c5673b2d3237509afdfba
dengchenggang.dcg <dengchenggang.dcg@alibaba-inc.com>
2026-02-12 15:01:10 +0800

    【XGUI】

---
+++ app/src/main/java/com/dcg/learnopengles/service/AccessibilityXGUIService.java
@@ -0,0 +1,18 @@
+package com.dcg.learnopengles.service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class AccessibilityXGUIService extends AccessibilityService {
+
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
